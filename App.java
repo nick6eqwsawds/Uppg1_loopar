b5018e9273199b19b042b961254dc8c2fd2ac096
otto hoffman <otto.hoffman@outlook.com>
2023-11-13 09:13:35 +0100

    del D

--- App.java
+++ App.java
@@ -1,7 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         int tal1=2;
+        int tal2=2;
+        //int tal1=0;
 
+        System.out.println("Del A-B");
         while(tal1<5){
             System.out.println(tal1);
             tal1++;
@@ -10,5 +13,10 @@ public class App {
         for(int i = 0; i < 5; i++){
             System.out.println(i);
         }
+        System.out.println("_ _ _ _ d) _ _ _ _ ");
+        do {
+            System.out.println(tal2);
+            tal2++;
+        } while(tal2<6);
     }
 }
