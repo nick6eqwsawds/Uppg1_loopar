31aabbbad8f737eb205f845a5a42a75a26b81eed
otto hoffman <otto.hoffman@outlook.com>
2023-11-13 08:58:11 +0100

    uppg1_loopar

---
+++ App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int tal1=0;
+
+        while(tal1<5){
+            System.out.println(tal1);
+            tal1++;
+        }
+    }
+}
